783e545f6a73e8c15de097ed5768fab9db582ef6
uxconor <conor.ux@gmail.com>
2022-08-28 14:35:31 -0400

    init comit

---
+++ auth/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('hi there');
+});
+
+app.listen(3000, () => {
+  console.log(`
+  🔑 Auth Service
+  📡 listening on port 3000
+  `);
+});
