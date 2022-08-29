12fbfd854f835cc1eee2e7a9708aee72b49c55f0
uxconor <conor.ux@gmail.com>
2022-08-29 11:38:37 -0400

    created signup route, and implemented error handling middleware

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log('something went wrong', err);
+
+  res.status(400).send({
+    message: 'something went wrong',
+  });
+};
