6d18695fae4f1862c1be9cd69af36c026d40fdf3
uxconor <conor.ux@gmail.com>
2022-09-06 17:16:17 -0400

    all tests passing for GET show ticket route

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,8 +1,15 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 
 it('returns a 404 if the ticket is not found', async () => {
-  await request(app).get('/api/tickets/djkaldfjalksfa').send().expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+  const response = await request(app)
+    .get(`/api/tickets/${id}`)
+    .send()
+    .expect(404);
+
+  console.log(response.body);
 });
 
 it('returns a ticket if the ticket is found', async () => {
@@ -22,6 +29,7 @@ it('returns a ticket if the ticket is found', async () => {
     .get(`/api/tickets/${response.body.id}`)
     .send()
     .expect(200);
+
   expect(ticketResponse.body.title).toEqual(title);
   expect(ticketResponse.body.price).toEqual(price);
 });
