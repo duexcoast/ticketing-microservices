3f0bb7d20453873330e3babe0549ce8d4e9c682c
uxconor <conor.ux@gmail.com>
2022-09-08 13:56:21 -0400

    abstracted classes for publisher

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,14 @@
 import { Listener } from './base-listener';
 import { Message } from 'node-nats-streaming';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message) {
-    console.log('Event data!', data);
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log('Event data:', data);
 
     msg.ack();
   }
