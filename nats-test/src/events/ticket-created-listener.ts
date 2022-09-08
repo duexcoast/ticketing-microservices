0297abbccc930f4556b791deeff9ce0020408251
uxconor <conor.ux@gmail.com>
2022-09-08 14:58:11 -0400

    added NATS helpers to common module

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,4 +1,4 @@
-import { Listener } from './base-listener';
+import { Listener } from '../../../common/src/events/base-listener';
 import { Message } from 'node-nats-streaming';
 import { TicketCreatedEvent } from './ticket-created-event';
 import { Subjects } from './subjects';
