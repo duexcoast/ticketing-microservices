6d5d2c0727f9e6051ba5e2aa665de0dc89fb771f
uxconor <conor.ux@gmail.com>
2022-09-08 16:48:30 -0400

    updated ticketing startup fn to gracefully shutdown NATS client on disconnection

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,7 +1,6 @@
 import { Listener } from '../../../common/src/events/base-listener';
 import { Message } from 'node-nats-streaming';
-import { TicketCreatedEvent } from './ticket-created-event';
-import { Subjects } from './subjects';
+import { TicketCreatedEvent, Subjects } from '@duexcoast/common';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
