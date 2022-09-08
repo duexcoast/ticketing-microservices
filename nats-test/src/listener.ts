09cdca40c69510016edb22931339ce3de3f04544
uxconor <conor.ux@gmail.com>
2022-09-08 12:22:26 -0400

    refactored into one file per class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -2,6 +2,8 @@
 import nats, { Message, Stan, SubscriptionOptions } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
+import { TicketCreatedListener } from './events/ticket-created-listener';
+
 console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -16,61 +18,9 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  new TicketCreatedListener(stan).listen()
+  new TicketCreatedListener(stan).listen();
 });
 // watching for interrupt signals
 process.on('SIGINT', () => stan.close());
 // watching for terminate signals
 process.on('SIGTERM', () => stan.close());
-
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
-  protected ackWait = 5 * 1000;
-
-  constructor(private client: Stan) {}
-
-  subscriptionOptions(): SubscriptionOptions {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-
-  listen(): void {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on('message', (msg: Message) => {
-      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
-
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-
-    return typeof data === 'string'
-      ? JSON.parse(data)
-      : JSON.parse(data.toString('utf8'));
-  }
-}
-
-class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
-  queueGroupName = 'payments-service';
-
-  onMessage(data: any, msg: Message) {
-    console.log('Event data!', data);
-
-    msg.ack();
-  }
-}
