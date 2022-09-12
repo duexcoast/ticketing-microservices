1a69757099622c7342b3d7f6ce7a67dae333e8ac
uxconor <conor.ux@gmail.com>
2022-09-12 16:52:07 -0400

    added env variables on tickets-depl for connecting to NATS client

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,14 +9,27 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
 
   try {
-    await natsWrapper.connect('ticketing', 'asdf', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
     });
-    
+
     process.on('SIGTERM', () => natsWrapper.client.close());
     process.on('SIGINT', () => natsWrapper.client.close());
 
@@ -32,5 +45,5 @@ const start = async () => {
     `);
   });
 };
- 
+
 start();
