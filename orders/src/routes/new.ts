8cad28d23333d8af1dbec93307c916da63eb3392
duexcoast <conor.ux@gmail.com>
2022-09-21 14:30:55 -0400

    created logic for finding existing orders in routes/new endpoint

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,16 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequests } from '@duexcoast/common';
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequests,
+  OrderStatus,
+  BadRequestError,
+} from '@duexcoast/common';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
 const router = express.Router();
 
 router.post(
@@ -16,6 +25,37 @@ router.post(
   ],
   validateRequests,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the DB.
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved.
+    // Run query to look at all orders. Find an order where the ticket is
+    // the ticket we just found, AND the order status is *not* canceled.
+    // If we find an order from this, that means the ticket *is* reserved.
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date for this order.
+
+    // Build the order and save it to the database.
+
+    // Publish an event saying that an order was created.
     res.send({});
   }
 );
