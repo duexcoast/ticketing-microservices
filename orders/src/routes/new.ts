e3ceffd718bd3a4d474cd4f5bfb99fe11b2951b3
duexcoast <conor.ux@gmail.com>
2022-09-21 12:15:55 -0400

    added routers to orders app.ts

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express';
-
+import { requireAuth, validateRequests } from '@duexcoast/common';
+import { body } from 'express-validator';
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [body('ticketId').not().isEmpty().withMessage('TicketId must be provided')],
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
